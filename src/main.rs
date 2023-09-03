125b171a0f703594f8f8e1928504389a3190addf
Simon Ledoux <simon@simon511000.fr>
2023-09-03 15:52:31 +0200

    Add automatic stop

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 use config::Config;
 use poise::serenity_prelude as serenity;
 use server::Server;
-use std::io::Result;
-use tokio::sync::Mutex;
+use std::{
+    io::Result,
+    sync::Arc,
+    time::{Duration, SystemTime},
+};
+use tokio::{sync::Mutex, time::interval};
 
 mod commands;
 mod config;
@@ -13,12 +17,38 @@ type Error = Box<dyn std::error::Error + Send + Sync>;
 type Context<'a> = poise::Context<'a, Data, Error>;
 
 pub struct Data {
-    server: Mutex<Server>,
+    server: Arc<Mutex<Server>>,
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let config = Config::new().unwrap();
+    let server = Arc::new(Mutex::new(Server::new(&config.server).await.unwrap()));
+    let server_clone = server.clone();
+
+    tokio::spawn(async move {
+        let mut interval = interval(Duration::from_secs(30));
+        let mut last_player_connected = SystemTime::now();
+
+        loop {
+            interval.tick().await;
+
+            // Verrouiller le Mutex du serveur
+            let mut server = server_clone.lock().await;
+
+            // Vérifier s'il n'y a aucun joueur connecté
+            if let Ok(nb_players) = server.nb_players().await {
+                if nb_players > 0 {
+                    last_player_connected = SystemTime::now();
+                    continue;
+                }
+
+                if last_player_connected.elapsed().unwrap() >= Duration::from_secs(300) {
+                    let _ = server.stop().await;
+                }
+            }
+        }
+    });
 
     poise::Framework::builder()
         .token(&config.token)
@@ -38,7 +68,7 @@ async fn main() -> Result<()> {
                 );
                 poise::builtins::register_globally(ctx, &framework.options().commands).await?;
                 Ok(Data {
-                    server: Mutex::new(Server::new(&config.server).await.unwrap()),
+                    server: server.clone(),
                 })
             })
         })
